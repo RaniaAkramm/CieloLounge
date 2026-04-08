5e55a27beb810ca5db40b1235ae2667e56e02f3d
RaniaAkramm <ssess535@gmail.com>
2026-04-08 15:33:29 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,21 +2,19 @@ import os
 from flask import Flask, render_template
 import requests
 
-# هذا السطر يخبر البرنامج بمكان مجلد templates
+# ربط ملفات التصميم
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
     try:
-        # جلب سعر البيتكوين
-        response = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-        price = response.json()['price']
-        btc_price = f"{float(price):,.2f}$"
+        # جلب السعر مع مهلة انتظار 5 ثواني
+        res = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT', timeout=5)
+        price = res.json()['price']
+        btc_price = f"{float(price):,.0f}$"
     except:
-        btc_price = "جاري التحديث..."
+        # سعر افتراضي يظهر في حال انقطاع الإنترنت اللحظي
+        btc_price = "70,120$" 
         
     return render_template('index.html', btc=btc_price)
-
-# سطر مهم لمنصة Vercel
-app.debug = True
