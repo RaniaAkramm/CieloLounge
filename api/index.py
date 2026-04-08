aae436228036f4ceb93ab2bf2a418cbb952286d4
RaniaAkramm <ssess535@gmail.com>
2026-04-08 16:50:16 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,19 +2,18 @@ import os
 from flask import Flask, render_template
 import requests
 
-# ربط ملفات التصميم
+# ربط المجلدات بشكل صحيح
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
     try:
-        # جلب السعر مع مهلة انتظار 5 ثواني
+        # جلب السعر العالمي
         res = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT', timeout=5)
         price = res.json()['price']
         btc_price = f"{float(price):,.0f}$"
     except:
-        # سعر افتراضي يظهر في حال انقطاع الإنترنت اللحظي
-        btc_price = "70,120$" 
+        btc_price = "70,120$" # سعر احترافي يظهر في حالة الطوارئ
         
     return render_template('index.html', btc=btc_price)
